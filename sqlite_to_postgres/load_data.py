d9d44e20aceb6b0ad3c5db0accb8d2d58bcb1439
Benrise <benrise_x@mail.ru>
2024-02-11 22:02:22 +0300

    Feat: base error handling on read/insert

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,6 +4,7 @@ import subprocess
 
 import psycopg2
 from psycopg2.extensions import connection as _connection
+from psycopg2 import DatabaseError
 from psycopg2.extras import DictCursor
 from utils.movie_dataclasses import (
     Genre,
@@ -43,9 +44,13 @@ class SQLiteExtractor:
         self.connection = connection
 
     def _execute_query(self, query):
-        curs = self.connection.cursor()
-        curs.execute(query)
-        return curs.fetchall()
+        try:
+            curs = self.connection.cursor()
+            curs.execute(query)
+            return curs.fetchall()
+        except sqlite3.Error as e:
+            print(f"SQLite error while executing the query: {e}")
+            raise
 
     def _get_tables(self):
         query = "SELECT * FROM sqlite_master WHERE type='table';"
@@ -54,46 +59,56 @@ class SQLiteExtractor:
     def _create_records(self, rows, record_class):
         records = []
         for row in rows:
-            record = record_class(*row)
-            records.append(record)
+            try:
+                record = record_class(*row)
+                records.append(record)
+            except Exception as e:
+                print(f"Error on creating a record: {e}")
+                raise
         return records
 
     def extract_movies(self):
-        film_work_data = []
-        genre_data = []
-        genre_film_work_data = []
-        person_data = []
-        person_film_work_data = []
+        try:
+            film_work_data = []
+            genre_data = []
+            genre_film_work_data = []
+            person_data = []
+            person_film_work_data = []
 
-        tables = self._get_tables()
-        for table in tables:
-            table_name = table[1]
-            query = f"SELECT * FROM {table_name}"
-            rows = self._execute_query(query)
+            tables = self._get_tables()
+            for table in tables:
+                table_name = table[1]
+                query = f"SELECT * FROM {table_name}"
+                rows = self._execute_query(query)
 
-            if (table_name == TablesNamesEnum.FILM_WORK.value):
-                film_work_data = self._create_records(rows, Filmwork)
+                if table_name == TablesNamesEnum.FILM_WORK.value:
+                    film_work_data = self._create_records(rows, Filmwork)
 
-            elif (table_name == TablesNamesEnum.GENRE.value):
-                genre_data = self._create_records(rows, Genre)
+                elif table_name == TablesNamesEnum.GENRE.value:
+                    genre_data = self._create_records(rows, Genre)
 
-            elif (table_name == TablesNamesEnum.GENRE_FILM_WORK.value):
-                genre_film_work_data = self._create_records(rows, GenreFilmwork)
+                elif table_name == TablesNamesEnum.GENRE_FILM_WORK.value:
+                    genre_film_work_data = self._create_records(rows, GenreFilmwork)
 
-            elif (table_name == TablesNamesEnum.PERSON.value):
-                person_data = self._create_records(rows, Person)
+                elif table_name == TablesNamesEnum.PERSON.value:
+                    person_data = self._create_records(rows, Person)
 
-            elif (table_name == TablesNamesEnum.PERSON_FILM_WORK.value):
-                person_film_work_data = self._create_records(rows, PersonFilmwork)
+                elif table_name == TablesNamesEnum.PERSON_FILM_WORK.value:
+                    person_film_work_data = self._create_records(rows, PersonFilmwork)
 
-        data_tables = DataTables(
-            DataTable(TablesNamesEnum.FILM_WORK.name, film_work_data),
-            DataTable(TablesNamesEnum.GENRE.name, genre_data),
-            DataTable(TablesNamesEnum.GENRE_FILM_WORK.name, genre_film_work_data),
-            DataTable(TablesNamesEnum.PERSON.name, person_data),
-            DataTable(TablesNamesEnum.PERSON_FILM_WORK.name, person_film_work_data))
+            data_tables = DataTables(
+                DataTable(TablesNamesEnum.FILM_WORK.name, film_work_data),
+                DataTable(TablesNamesEnum.GENRE.name, genre_data),
+                DataTable(TablesNamesEnum.GENRE_FILM_WORK.name, genre_film_work_data),
+                DataTable(TablesNamesEnum.PERSON.name, person_data),
+                DataTable(TablesNamesEnum.PERSON_FILM_WORK.name, person_film_work_data))
 
-        return data_tables
+            return data_tables
+
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise
+
 
 
 class PostgresSaver:
@@ -111,21 +126,32 @@ class PostgresSaver:
 
 
 def insert_table_records(data_table: DataTable, curs: DictCursor):
-    args = []
-    attribute_names = []
-    for item in data_table.data:
-        item_fields = fields(item)
-        attribute_names = [field.name for field in item_fields]
-        values = [getattr(item, field) for field in attribute_names]
-        args.append(curs.mogrify(f"({', '.join(['%s']*len(attribute_names))})", tuple(values)).decode())
-    args_str = ','.join(args)
-    attribute_names_str = ', '.join(attribute_names)
+    try:
+        args = []
+        attribute_names = []
+        for item in data_table.data:
+            item_fields = fields(item)
+            attribute_names = [field.name for field in item_fields]
+            values = [getattr(item, field) for field in attribute_names]
+            args.append(curs.mogrify(f"({', '.join(['%s']*len(attribute_names))})", tuple(values)).decode())
+        args_str = ','.join(args)
+        attribute_names_str = ', '.join(attribute_names)
 
-    curs.execute(f"""
-        INSERT INTO content.{data_table.name} ({attribute_names_str})
-        VALUES {args_str}
-        ON CONFLICT (id) DO NOTHING
-    """)
+        curs.execute(f"""
+            INSERT INTO content.{data_table.name} ({attribute_names_str})
+            VALUES {args_str}
+            ON CONFLICT (id) DO NOTHING
+        """)
+
+        curs.connection.commit()
+
+    except DatabaseError as e:
+        print(f"Database error while insert data: {e}")
+        curs.connection.rollback()
+
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
